57f5bf402618aea0c7d49620aef418b6b90c597c
Abdalla Diaai <abdalla.diaai@outlook.com>
2024-12-20 22:11:30 +0200

    finished RoomJoin component.

--- music/api/urls.py
+++ music/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('room', views.RoomView.as_view(), name='room'),
     path('create-room', views.CreateRoomView.as_view(), name='create-room'),
     path('get-room', views.GetRoom.as_view(), name='get-room'),
+    path('join-room', views.JoinRoom.as_view(), name='join-room'),
 
 ]
--- music/frontend/src/components/RoomJoin.js
+++ music/frontend/src/components/RoomJoin.js
@@ -1,13 +1,60 @@
 import React, { useState } from "react";
-import { Button, Grid, Typography, TextField, FormControl, FormHelperText, Radio, RadioGroup, FormControlLabel, FormLabel } from "@mui/material";
+import { Button, Grid, Typography, TextField, } from "@mui/material";
 import { useNavigate, Link } from "react-router-dom";
 
 const RoomJoin = () => {
+
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+
+
+  const roomButtonPressed = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code: roomCode }),
+    };
+    const response = await fetch("/api/join-room", requestOptions);
+    if (response.ok) {
+      navigate(`/room/${roomCode}`);
+    } else {
+      setError("Room not found.");
+    }
+  };
+
   return (
-    <div>
-      <h1>Room Join</h1>
-      <p>Select A Room: </p>
-    </div>
+    <Grid container spacing={1} alignItems={"center"}>
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          error={!!error}
+          label="Code"
+          placeholder="Enter a Room Code"
+          value={roomCode}
+          onChange={(e) => setRoomCode(e.target.value)}
+          helperText=" "
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={roomButtonPressed}>
+          Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="secondary" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+
+    </Grid>
+
   );
 };
 
